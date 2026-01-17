884c14e12d875e50fd6943c288c31342ab765eb2
Matthew Yang <mattyang1340@gmail.com>
2026-01-17 13:52:00 +1100

    docs(utils): remove safe_direct_message docstring

--- src/utils/direct_message.py
+++ src/utils/direct_message.py
@@ -2,9 +2,6 @@ from discord import Member
 
 
 async def safe_direct_message(member: Member, message: str) -> Exception | None:
-    """
-    Returns empty string if successful, otherwise False
-    """
     try:
         await member.send(message)
     except Exception as e:
